6448c4792a9ee17547698048a6da52ff6647c1a3
soft-compile <ezyjsdeveloper@gmail.com>
2026-05-09 08:59:50 +0800

    more func

--- main.cpp
+++ main.cpp
@@ -9,27 +9,48 @@ namespace fs = std::filesystem;
 int main(int argc, char *argv[])
 {
     argv_verify verify("CTFFuck2");
-    verify.append("-f", "--file", "The program file");
+    verify.append("-f", "--file", "The program file", "", {"-r"});
     verify.append("--debug", "--debug", "Trigger debug mode", "false");
+    verify.append("-r", "--run", "Run an execution to the given code", "", {"-f"});
     std::unordered_map<std::string, std::string> args = verify.verify(argv, argc);
     bool debug = false;
     if (args.at("--debug") != "false")
     {
         debug = true;
     }
-    const std::string FILE_PATH = args.at("-f");
-    if (!(fs::exists(FILE_PATH) && fs::is_regular_file(FILE_PATH)))
+    if (args.at("-r").size())
     {
-        std::cerr << "Error occurred when trying to access script file \"" << FILE_PATH << "\", path points to non-regular file." << std::endl;
-        return -1;
+        run(args.at("-r"), debug);
     }
-    std::ifstream file(FILE_PATH);
-    if (!file.is_open())
+    else
     {
-        std::cerr << "Error occurred when trying to open script file \"" << FILE_PATH << "\"." << std::endl;
-        return -1;
+        const std::string FILE_PATH = args.at("-f");
+        if (FILE_PATH == "stdin")
+        {
+            std::string line;
+            std::string total;
+            while (std::getline(std::cin, line))
+            {
+                total += line;
+            }
+            run(total, debug);
+        }
+        else
+        {
+            if (!(fs::exists(FILE_PATH) && fs::is_regular_file(FILE_PATH)))
+            {
+                std::cerr << "Error occurred when trying to access script file \"" << FILE_PATH << "\", path points to non-regular file." << std::endl;
+                return -1;
+            }
+            std::ifstream file(FILE_PATH);
+            if (!file.is_open())
+            {
+                std::cerr << "Error occurred when trying to open script file \"" << FILE_PATH << "\"." << std::endl;
+                return -1;
+            }
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            run(buffer.str(), debug);
+        }
     }
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    run(buffer.str(), debug);
 }
