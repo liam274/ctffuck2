276e4c101ccef4d9fe2de110e6d50b9f42e57e40
soft-compile <ezyjsdeveloper@gmail.com>
2026-05-13 19:21:25 +0800

    minize

--- lib/interpreter.h
+++ lib/interpreter.h
@@ -5,8 +5,9 @@
 #include <array>
 #include <algorithm>
 #include <sstream>
+#include <termios.h>
+#include <unistd.h>
 #include "data-type.h"
-#include "algorithm.h"
 
 constexpr short MEMORY_SIZE = 10;
 constexpr short read_stack_length = 1, add_stack_length = 1,
@@ -114,6 +115,31 @@ inline void print_debug(Ctx *ctx, int chr_int, int last)
     std::cout << '\n';
     std::cout << std::flush;
 }
+
+class TermiosRaw
+{
+private:
+    struct termios orig_;
+    int fd_;
+
+public:
+    explicit TermiosRaw(int fd = STDIN_FILENO) : fd_(fd)
+    {
+        tcgetattr(fd_, &orig_);
+        struct termios raw = orig_;
+        raw.c_lflag &= ~(ICANON | ECHO);
+        raw.c_cc[VMIN] = 1;
+        raw.c_cc[VTIME] = 0;
+        tcsetattr(fd_, TCSANOW, &raw);
+    }
+
+    ~TermiosRaw()
+    {
+        tcsetattr(fd_, TCSANOW, &orig_);
+    }
+    TermiosRaw(const TermiosRaw &) = delete;
+    TermiosRaw &operator=(const TermiosRaw &) = delete;
+};
 
 inline std::array<int, MEMORY_SIZE> run(const std::string &data, const bool debug, std::string input = "")
 {
